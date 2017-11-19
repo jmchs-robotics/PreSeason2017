2bc15421ad40e919d94168d0137c8b2a7f2db32d
Goodboyjohnn <ewhall18@gmail.com>
2017-11-19 16:25:52 -0700

    Mostly some drivetrain work
    Code before testing for a P (proportion) controller in the
    DriveStraightGyro Command is finished. A matching constant has been
    added to the drivetrain class. More could be done to change the gyro
    drive straight, e.g. finishing on a bump (reading accelerometer) or by
    vision processing (speaking of which, needs to be added and
    implemented).

---
+++ PreSeason2017/src/org/usfirst/frc5933/PreSeason2017/commands/AutonomousGroup.java
@@ -0,0 +1,68 @@
+package org.usfirst.frc5933.PreSeason2017.commands;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class AutonomousGroup extends CommandGroup {
+
+    public AutonomousGroup() {
+        // Add Commands here:
+        // e.g. addSequential(new Command1());
+        //      addSequential(new Command2());
+        // these will run in order.
+
+        // To run multiple commands at the same time,
+        // use addParallel()
+        // e.g. addParallel(new Command1());
+        //      addSequential(new Command2());
+        // Command1 and Command2 will run in parallel.
+
+        // A command group will require all of the subsystems that each member
+        // would require.
+        // e.g. if Command1 requires chassis, and Command2 requires arm,
+        // a CommandGroup containing them would require both the chassis and the
+        // arm.
+    	
+    	/*
+    	 * the order of events in autonomous is:
+    	 * 1. drive forward to the flag.
+    	 * 2. grab the flag.
+    	 * 3. drive backwards to the flag zone.
+    	 * 4. release flag
+    	 * 5. turn away from flag
+    	 * 6. drive away from flag
+    	 * 
+    	 * These can be further divided into:
+    	 * 1. gyro drive forward to about 5 feet of the flag
+    	 * 2. use vision processing to drive to the flag
+    	 * 3. grab the flag
+    	 * 4. gyro drive backward to the flag zone
+    	 * 5. release flag
+    	 * 6. turn away from flag
+    	 * 7. drive away from flag
+    	 * 
+    	 * Note that timings are going to be inconsistent due to the variable nature of the field.
+    	 * Basically, because the field is bumpy, we can't time it so well.
+    	 */
+    	
+    	//drive forward to the flag
+    	addSequential(new SimpleDriveStraight());
+    	
+    	//grab the flag
+    	addSequential(new CloseAllClaws());
+    	
+    	//drive backwards to the flag zone
+    	addSequential(new SimpleDriveStraight());
+    	
+    	//release the flag
+    	addSequential(new OpenAllClaws());
+    	
+    	//turn away from the flag
+    	addSequential(new SimpleTurn());
+    	
+    	//drive away from the flag
+    	addSequential(new SimpleDriveStraight());
+    }
+}

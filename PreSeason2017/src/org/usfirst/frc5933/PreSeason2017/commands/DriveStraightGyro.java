03f3a422b05071c2cf34a4b8fcc48524574421c8
Goodboyjohnn <ewhall18@gmail.com>
2017-11-21 17:36:38 -0700

    Cleaned commands
    Made sure that algorithms made sense and that interrupted() called
    end().

--- PreSeason2017/src/org/usfirst/frc5933/PreSeason2017/commands/DriveStraightGyro.java
+++ PreSeason2017/src/org/usfirst/frc5933/PreSeason2017/commands/DriveStraightGyro.java
@@ -79,5 +79,6 @@ public class DriveStraightGyro extends Command {
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	end();
     }
 }
